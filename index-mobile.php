6e04f8716c0007aaf9646be07ec6e3f4c7668b37
Ralph Christopher Castillo <rapi.castillo@gmail.com>
2014-12-17 13:36:36 -0500

    Mobile

--- index-mobile.php
+++ index-mobile.php
@@ -2,29 +2,156 @@
 <head>
    <link rel='stylesheet' type='text/css' href='style/bootstrap.css'/>
     <link rel='stylesheet' type='text/css' href='style/bootstrap-theme.css'/>
-    <link rel='stylesheet' type='text/css' href='style/custom2.css'/>
+    <link rel='stylesheet' type='text/css' href='css/whdt-mobile.css'/>
     <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700,800'/>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
 </head>
 <body style="background-image: url(img/branch-background.png); background-repeat: no-repeat;">
-  <div class="fluid-container">
-    <div class="row">
-      <div class="col-md-12">
-        <h1>
-          Mobile Version of the report, coming soon
-        </h1>
-        <h3>
-          <a href="downloads/World Humanitarian Data and Trends 2014.pdf">In the meantime download the report here.</a>
-        </h3>
+  <div class="container-fluid">
+
+    <!-- 1 ) Title Area-->
+    <div class='row' id='slide-1-title'>
+      <div class='col-sm-12 col-xs-12'>
+        <p><img src='mob/1-title.png' width="80%"></p>
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-12">
-        <h2 class="dark-color">
-          WORLD HUMANITARIAN
-        </h2>
-        <h2 class="color-red">
-          DATA AND TRENDS 2014
-        </h2>
+    <div class='row red-title' id='slide-2a-title'>
+      <div class='col-sm-12 col-xs-12'>
+        <h3>The context of humanitarian crises is changing</h3>
+      </div>
+    </div>
+    <div class='row bg1-content' id='slide-2a-content'>
+      <div class='col-sm-12 col-xs-12'>
+        <p>In 2013 the humanitarian community was confronted with emergencies of unprecedented magnitude.</p>
+        <p>This site - based on the publication - shows the impact of natural disasters and conflict on the millions of people affected worlwide, the changing nature of the crises and looks at the critical role that partnerships can play in meeting the needs of affected people.</p>
+        <p><img src='mob/2-chart.png' width="100%"/></p>
+      </div>
+    </div>
+    <div class='row red-title' id='slide-3-title'>
+      <div class='col-sm-12 col-xs-12'>
+        <h3>people affected by <span class='highlight'>conflicts</span> and <span class='highlight'>natural disasters</span> in 2013</h3>
+      </div>
+    </div>
+    <div class='row bg2-content' id='slide-3-content'>
+      <div class='col-sm-12 col-xs-12'>
+        <div class='row'>
+          <div class='col-sm-4 col-xs-5 text-right'><h1 class='really-big red'>97</h1></div>
+          <div class='col-sm-8 col-xs-7'>
+            <h2 class='red fig-subtitle'>million</h2>
+            <h4 class='black'>people affected by <span class='blue'>natural disasters</span> in 2013</h4>
+          </div>
+        </div>
+        <div class='row affected-natl'>
+          <div class='col-sm-6 col-xs-6 text-center'><img src='mob/3-china.png'></div>
+          <div class='col-sm-6 col-xs-6 text-center'><img src='mob/3-philippines.png'></div>
+        </div>
+        <div class='row  affected-natl'>
+          <div class='col-sm-6 col-xs-6 text-center'><img src='mob/3-india.png'></div>
+          <div class='col-sm-6 col-xs-6 text-center'><img src='mob/3-vietnam.png'></div>
+        </div>
+        <div class='row  affected-natl'>
+          <div class='col-sm-6 col-xs-6 text-center'><img src='mob/3-thailand.png'></div>
+          <div class='col-sm-6 col-xs-6 blue-bg'><p><strong>Top five countries by number of people affected by natural disasters</strong></p></div>
+        </div>
+      </div>
+    </div>
+    <div class='row bg2-content-inverted' id='slide-3-content-cont'>
+      <div class='col-sm-12'>
+        <div class='row'>
+          <div class='col-sm-4 col-xs-5 text-right'><h1 class='really-big red'>51.2</h1></div>
+          <div class='col-sm-8 col-xs-7'>
+            <h2 class='red fig-subtitle'>million</h2>
+            <h4 class='black'>people affected by <span class='blue'>conflict</span> in 2013</h4>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12'>
+            <h4><span class='red'>Asylum Seekers:</span> <span class='black'>1.2 million</span></h4>
+          </div>
+          <div class='col-sm-12 people-area'>
+            <?php for ($i = 0; $i < 12; $i++): ?>
+              <img src='mob/4-people.png' />
+            <?php endfor; ?>
+          </div>
+          <div class='row'><div class='col-sm-12'>&nbsp;</div></div>
+          <div class='col-sm-12'>
+            <h4><span class='red'>Internally displaced people:</span> <span class='black' style='display: inline-block'>33.3 million</span></h4>
+          </div>
+          <div class='col-sm-12 people-area'>
+            <?php for ($i = 0; $i < 333; $i++): ?>
+              <img src='mob/4-people.png'/>
+            <?php endfor; ?>
+          </div>
+          <div class='row'><div class='col-sm-12'>&nbsp;</div></div>
+          <div class='col-sm-12'>
+            <h4><span class='red'>Refugees:</span> <span class='black' style='display: inline-block'>16.7 million</span></h4>
+          </div>
+          <div class='col-sm-12 people-area'>
+            <?php for ($i = 0; $i < 167; $i++): ?>
+              <img src='mob/4-people.png' />
+            <?php endfor; ?>
+          </div>
+        </div>
+        <div class='row'><div class='col-sm-12'>&nbsp;</div></div>
+        <div class='row'>
+          <div class='col-sm-12 blue-bg text-center'>
+            <h4 class='white'>16% increase in IDPs</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class='row red-title'>
+      <div class='col-sm-12'>
+        <h3>Crises are longer and more expensive</h3>
+      </div>
+    </div>
+    <div class='row' id='crises-more-expensive'>
+      <div class='col-sm-12'>
+        <div class='row'>
+          <div class='col-sm-12'>
+            <p class='text-center'>
+              In the last ten years, the funding requirements of interagency appeals have increased by 600% from $3 billion in 2004 to $17.9 billion in 2014.
+            </p>
+            <p class='text-center'>
+              In the same amount of time, the number of people targeted for assistance has more than doubled.
+            </p>
+          </div>
+        </div>
+        <div class='row'><div class='col-sm-12'>&nbsp;</div></div>
+        <div class='row'>
+          <div class='col-sm-3 col-xs-3'></div>
+          <div class='col-sm-3 col-xs-3'><img src='mob/5-3b.png' width='100%' /></div>
+          <div class='col-sm-3 col-xs-3'><img src='mob/5-17.9b.png' width='100%' /></div>
+          <div class='col-sm-3 col-xs-3'></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- PARTNERSHIPS -->
+    <div class='row red-title'>
+      <div class='col-sm-12'>
+        <h3>Why are partnerships important?</h3>
+      </div>
+    </div>
+    <div class='row partnerships-bg'>
+      <div class='col-sm-12'>
+        <div class='row'>
+          <div class='col-sm-3 col-xs-3'></div>
+          <div class='col-sm-6 col-xs-6 text-center'>
+            <img src='mob/6-twitter.png' width='90%' />
+            <h4 class='white'>Make the most out of new opportunities</h4>
+            <br/>
+            <img src='mob/6-house.png' width='90%' />
+            <h4 class='white'>Overcome challenges and build capacity</h4>
+            <br/>
+            <img src='mob/6-military.png' width='90%' />
+            <h4 class='white'>Deliver Effectively</h4>
+          </div>
+          <div class='col-sm-3 col-xs-3'></div>
+        </div>
       </div>
     </div>
   </div>
@@ -33,6 +160,21 @@
   <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
   <script src="js/skrollr.min.js"></script>
   <script src="js/skrollr-ie.min.js"></script>
+  <script src="js/jquery-1.11.1.min.js"></script>
+  <script type='text/javascript'>
+  $(function() {
+     $('#slide-1-title').height(screen.height);
+  });
+  </script>
+  <script  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  >
 
+    ga('create', 'UA-1433955-15', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 </body>
 </html>
