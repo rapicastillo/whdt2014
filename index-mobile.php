7f2322174c68b3d98c226baeb5a711f72e7b9435
Ralph Christopher Castillo <rapi.castillo@gmail.com>
2014-12-29 12:16:52 -0500

    Fixes

--- index-mobile.php
+++ index-mobile.php
@@ -434,11 +434,11 @@
      $('#slide-1-title').height(screen.height);
   });
   </script>
-  <script  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
->
 
   ga('create', 'UA-1433955-15', 'auto');
   ga('send', 'pageview');
